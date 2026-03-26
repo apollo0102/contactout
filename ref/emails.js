7571101c132560f1301d2e9378b670376abaffd6
apollo0102 <crazyprince0012@gmail.com>
2026-03-26 14:36:04 -0400

    update email list

--- ref/emails.js
+++ ref/emails.js
@@ -1,7 +1,18 @@
+// export const EMAIL_USER_LIST = [
+//   "twin.rabbit0617@gmail.com",
+//   "tainuepolob@gmail.com",
+//   "isbsusigu@gmail.com",
+// ];
+
+// export const CONTACTOUT_PASSWORD = "Sharkham617@";
+
+
 export const EMAIL_USER_LIST = [
-  "twin.rabbit0617@gmail.com",
-  "tainuepolob@gmail.com",
-  "isbsusigu@gmail.com",
+    "cierasummeier283101@outlook.com",
+    "chrisslater0231@outlook.com",
+    "macireando@outlook.com",
+    "meaganrainier2026@outlook.com",
+    "AshleyW2123@outlook.com"
 ];
 
-export const CONTACTOUT_PASSWORD = "Sharkham617@";
+export const CONTACTOUT_PASSWORD = "w123456789*";
